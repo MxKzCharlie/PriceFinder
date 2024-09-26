82537eb829e4f44dbb9e0d9578f6e6acc6a9457a
Carlos <mxkcharlie01@gmail.com>
2024-09-26 13:14:59 -0700

    csrf token commit

--- src/components/functions/cookiesCSRF.js
+++ src/components/functions/cookiesCSRF.js
@@ -2,18 +2,18 @@ function getTokenCSRF(name){
     let cookieValue = null;
 
     if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
+    const cookies = document.cookie.split(';');
 
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 export default getTokenCSRF;
